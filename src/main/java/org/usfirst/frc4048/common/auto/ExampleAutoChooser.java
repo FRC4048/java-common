28baaf8c76149dccc3c294a91694e042bd8a704f
goatfanboi23 <python.goat@gmail.com>
2023-12-11 00:54:10 -0500

    fixed missed functionality and removed unneeded interface

--- src/main/java/org/usfirst/frc4048/common/auto/ExampleAutoChooser.java
+++ src/main/java/org/usfirst/frc4048/common/auto/ExampleAutoChooser.java
@@ -1,19 +1,31 @@
 package org.usfirst.frc4048.common.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.Map;
 
 public class ExampleAutoChooser extends GenericAutoChooser{
-     @Override
-     public AutoAction getDefaultOption() {
-          return AutoAction.DoNothing;
-     }
 
      @Override
      Map<AutoEventComparer, Command> getCommandMap() {
           return Map.of(AutoEventComparer.fromAction(AutoAction.DoNothing), new PlaceHolderCommand(),
                   AutoEventComparer.fromActionAndLocation(AutoAction.TwoPieceMoveLeft, FieldLocation.Middle), new PlaceHolderCommand());
      }
-     
+
+     @Override
+     CommandBase getDefaultCommand() {
+          return new PlaceHolderCommand();
+     }
+
+     @Override
+     AutoAction getDefaultActionOption() {
+          return AutoAction.DoNothing;
+     }
+
+     @Override
+     FieldLocation getDefaultLocationOption() {
+          return FieldLocation.Middle;
+     }
+
 }
--- src/main/java/org/usfirst/frc4048/common/auto/GenericAutoChooser.java
+++ src/main/java/org/usfirst/frc4048/common/auto/GenericAutoChooser.java
@@ -2,28 +2,24 @@ package org.usfirst.frc4048.common.auto;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import javax.swing.*;
 import java.util.*;
 
-public abstract class GenericAutoChooser implements AutoChooser {
+public abstract class GenericAutoChooser {
      private SendableChooser<AutoAction> actionChooser;
      private SendableChooser<FieldLocation> locationChooser;
 
      public GenericAutoChooser() {
           actionChooser = new SendableChooser<>();
           locationChooser = new SendableChooser<>();
-     }
-
-     public void init() {
-          setOptions();
-          actionChooser.setDefaultOption(getDefaultOption().getName(), getDefaultOption());
-     }
-
-     @Override
-     public void setOptions() {
           Arrays.stream(AutoAction.values()).forEach(a -> actionChooser.addOption(a.getName(), a));
           Arrays.stream(FieldLocation.values()).forEach(l -> locationChooser.addOption(l.name(), l));
+          actionChooser.setDefaultOption(getDefaultActionOption().getName(), getDefaultActionOption());
+          locationChooser.setDefaultOption(getDefaultLocationOption().name(), getDefaultLocationOption());
      }
+     
 
      public AutoAction getAction() {
           return actionChooser.getSelected() == null ? AutoAction.DoNothing : actionChooser.getSelected();
@@ -32,14 +28,16 @@ public abstract class GenericAutoChooser implements AutoChooser {
      public FieldLocation getLocation() {
           return locationChooser.getSelected() == null ? FieldLocation.Middle : locationChooser.getSelected();
      }
-
-     @Override
+     
      public Command getAutoCommand() {
           Optional<AutoEventComparer> first = getCommandMap().keySet().stream().filter(event -> event.isAutoEventValid(new AutoEvent(getAction(), getLocation()))).findFirst();
           if (first.isPresent()) return getCommandMap().get(first.get());
-          else return new PlaceHolderCommand(); //default command
+          else return getDefaultCommand();
      }
 
      abstract Map<AutoEventComparer, Command> getCommandMap();
+     abstract CommandBase getDefaultCommand();
+     abstract AutoAction getDefaultActionOption();
+     abstract FieldLocation getDefaultLocationOption();
 
 }
