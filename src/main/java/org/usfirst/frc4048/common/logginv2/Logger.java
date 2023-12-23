6d97513b33ed3c80629fb936a243d5534b3a7356
Noah <dev.codetoad@gmail.com>
2023-12-22 22:14:35 -0500

    cant really test right now, but hypothetical it should print when command group starts and ends

--- src/main/java/org/usfirst/frc4048/common/logginv2/Logger.java
+++ src/main/java/org/usfirst/frc4048/common/logginv2/Logger.java
@@ -1,12 +1,27 @@
 package org.usfirst.frc4048.common.logginv2;
 
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Logger {
+    //command Groups
+
+    @Before("execution(* edu.wpi.first.wpilibj2.command.CommandGroupBase+.initialize(..))")
+    public void logCommandGroupInit(JoinPoint joinPoint){
+        System.out.println("INIT");
+    }
+    @Before("execution(* edu.wpi.first.wpilibj2.command.CommandGroupBase+.end(..))")
+    public void logCommandGroupEnd(JoinPoint joinPoint){
+        System.out.println("END");
+    }
+
+    //commands
     @Pointcut("within( @org.usfirst.frc4048.common.logginv2.LoggedCommand org.usfirst.frc4048.common.logginv2.commands..*)")
     public void validCommand(){}
 
