fa0ada62eeec9b15cb6363ee14f91197f992bab0
codetoad <softwareenginer@pm.me>
2024-12-03 10:47:55 -0500

    added some docs

--- src/main/java/org/usfirst/frc4048/common/swervev3/bags/OdometryMeasurement.java
+++ src/main/java/org/usfirst/frc4048/common/swervev3/bags/OdometryMeasurement.java
@@ -2,5 +2,10 @@ package org.usfirst.frc4048.common.swervev3.bags;
 
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
+/**
+ * Class containing odometry state for a given measurement.
+ * @param modulePosition Array of swerve module positions for a single reading in the order [FrontLeft, FrontRight, BackLeft, BackRight]
+ * @param gyroValueDeg gyro value in degrees during the reading
+ */
 public record OdometryMeasurement(SwerveModulePosition[] modulePosition, double gyroValueDeg) {
 }
--- src/main/java/org/usfirst/frc4048/common/swervev3/bags/VisionMeasurement.java
+++ src/main/java/org/usfirst/frc4048/common/swervev3/bags/VisionMeasurement.java
@@ -3,5 +3,10 @@ package org.usfirst.frc4048.common.swervev3.bags;
 import edu.wpi.first.math.geometry.Pose2d;
 import org.usfirst.frc4048.common.util.Apriltag;
 
+/**
+ * @param measurement Estimated position of robot received from vision
+ * @param tag the AprilTag that the robot saw
+ * @param timeOfMeasurement timestamp in seconds that the vision pose was RECORDED
+ */
 public record VisionMeasurement(Pose2d measurement, Apriltag tag, double timeOfMeasurement)  {
 }
