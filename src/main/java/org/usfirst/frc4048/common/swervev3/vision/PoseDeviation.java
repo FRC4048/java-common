f1c5989469fa00b1333a389084e286a754135c89
codetoad <softwareenginer@pm.me>
2024-12-13 10:23:38 -0500

    added PoseDeviation doc

--- src/main/java/org/usfirst/frc4048/common/swervev3/vision/PoseDeviation.java
+++ src/main/java/org/usfirst/frc4048/common/swervev3/vision/PoseDeviation.java
@@ -3,6 +3,9 @@ package org.usfirst.frc4048.common.swervev3.vision;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.numbers.N3;
 
+/**
+ * POJO containing wheel encoder standard deviation and vision standard deviation
+ */
 public class PoseDeviation {
     private final Vector<N3> wheelStd;
     private final Vector<N3> visionStd;
