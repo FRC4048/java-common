29389c249a891c8429e402ff77c37563c908222f
codetoad <softwareenginer@pm.me>
2025-01-12 14:07:54 -0500

    added math utils

---
+++ src/main/java/org/usfirst/frc4048/common/util/math/RangeUtils.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc4048.common.util.math;
+
+public class RangeUtils {
+    public static double map(double value, double startMin, double startMax, double endMin, double endMax){
+        double endRange = endMax - endMin;
+        double startRange = startMax - startMin;
+        return ((value - startMin) * endRange / startRange) + endMin;
+    }
+}
